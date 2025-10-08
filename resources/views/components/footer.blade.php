d1938c3a8b8163888522128ed855fa4e273d22fb
alfarinsqi <alfarinsqin@gmail.com>
2025-10-08 20:52:34 +0700

    Selesai Langkah Praktikum Modul 5

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,4 @@
 <footer style="background-color: #6F4E37; padding: 15px; color: white; text-align: center; position:fixed; bottom:0; width:100%;">
     © 2025 BioMahasiswa | All Rights Reserved
 </footer>
+
